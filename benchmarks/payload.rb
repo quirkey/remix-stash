1fb19184f4238e0abdd583069813cff51a8afc51
Brian Mitchell <brian.mitchell@excoventures.com>
2009-09-04 09:54:45 -0400

    Add memcache-client comparisons.
    This benchmark starts to show how GC becomes a huge issue in Ruby. Just
    loading Rubygems causes remix-stash to take over a 45% hit on my machine.
    This is because most of the time is spent GCing not executing on 1.8. 1.9
    does slightly better but still gets at least a 40% hit on large payloads.

--- benchmarks/payload.rb
+++ benchmarks/payload.rb
@@ -1,6 +1,6 @@
 require 'benchmark'
 require File.dirname(__FILE__) + '/../harness'
-# require File.dirname(__FILE__) + '/../harness_cache'
+require File.dirname(__FILE__) + '/../harness_cache'
 
 LARGE_NUMBER = 20_000
 
@@ -10,28 +10,52 @@ small_value = 'c' * 100
 tiny_value = 'd'
 
 Benchmark.bm do |b|
-  b.report('100k') do
+  b.report('100k stash') do
     LARGE_NUMBER.times {
       stash.write('x', large_value)
       stash.read('x')
     }
   end
-  b.report('2k') do
+  b.report('100k cache') do
+    LARGE_NUMBER.times {
+      Cache.set('x', large_value, 0, true)
+      Cache.get('x', true)
+    }
+  end
+  b.report('2k stash') do
     LARGE_NUMBER.times {
       stash.write('x', med_value)
       stash.read('x')
     }
   end
-  b.report('100 bytes') do
+  b.report('2k cache') do
+    LARGE_NUMBER.times {
+      Cache.set('x', med_value, 0, true)
+      Cache.get('x', true)
+    }
+  end
+  b.report('100b stash') do
     LARGE_NUMBER.times {
       stash.write('x', small_value)
       stash.read('x')
     }
   end
-  b.report('1 byte') do
+  b.report('100b cache') do
+    LARGE_NUMBER.times {
+      Cache.set('x', small_value, 0, true)
+      Cache.get('x', true)
+    }
+  end
+  b.report('1b stash') do
     LARGE_NUMBER.times {
       stash.write('x', tiny_value)
       stash.read('x')
+    }
+  end
+  b.report('1b cache') do
+    LARGE_NUMBER.times {
+      Cache.set('x', tiny_value, 0, true)
+      Cache.get('x', true)
     }
   end
 end
