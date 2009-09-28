500d6c5002f4ebef21a9be5d4aeae982aecdae0b
Brian Mitchell <brian.mitchell@excoventures.com>
2009-09-28 15:55:58 -0400

    Allow spec.rb to be used as a spec runner.

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,10 +1,15 @@
-require 'rubygems'
-require 'test/unit'
-require 'shoulda'
+if __FILE__ == $0
+  base = File.dirname(__FILE__)
+  Dir[base + '/*_spec.rb'].each {|f| require f}
+else
+  require 'rubygems'
+  require 'test/unit'
+  require 'shoulda'
 
-$LOAD_PATH << File.dirname(__FILE__) + '/../lib'
-require 'remix/stash'
+  $LOAD_PATH << File.dirname(__FILE__) + '/../lib'
+  require 'remix/stash'
 
-Spec = Test::Unit::TestCase
+  Spec = Test::Unit::TestCase
 
-include Remix
+  include Remix
+end
